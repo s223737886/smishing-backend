1bc370a4e0bb5282abd7b195d45bb5611f35eec3
Kalpna Dixit <s223737886@deakin.edu.au>
2025-04-02 22:25:04 +1100

    Save changes on MongoDB connection issue

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('Connected to MongoDB')
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log('✅ Connected to MongoDB')
     } catch (error) {
-        console.error('MongoDB connection error:', error)
+        console.error('❌ MongoDB connection error:', error)
         process.exit(1) // Exit if database connection fails
     }
 }
